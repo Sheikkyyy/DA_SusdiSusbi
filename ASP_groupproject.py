000f12a5f2987a19a419d81abf919afce005447f
Sheikkyyy <harishbawthan@gmail.com>
2023-02-20 22:03:19 +0800

    ASP_Project

--- ASP_groupproject.py
+++ ASP_groupproject.py
@@ -2,10 +2,12 @@ import pandas as pd
 import numpy as np
 import matplotlib as plt
 
-df = pd.read_excel(r'C:\Users\haris\PycharmProjects\ASP_Labs\PYProjectNew\Project_File.xlsx')
+dataset = pd.read_excel(r'C:\Users\haris\PycharmProjects\ASP_Labs\PYProjectNew\Project_File.xlsx')
 
-print(df)
-
+print(dataset)
+#renaming the first column to date
+dataset_1 = dataset.rename(columns={'   ': 'Date'})
+print(dataset_1)
 class calSusbisusdi:
     def __init__(self):
         self.country = []
@@ -20,4 +22,5 @@ class calSusbisusdi:
         country = []
         date = []
         avgvisitor = []
+
 

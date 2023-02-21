2ae492ade3106f8b32edd8b81695295f6431d96a
Sheikkyyy <harishbawthan@gmail.com>
2023-02-21 13:21:09 +0800

    ASP_Project

--- ASP_groupproject.py
+++ ASP_groupproject.py
@@ -5,7 +5,23 @@ import matplotlib as plt
 dataset = pd.read_excel(r'C:\Users\haris\PycharmProjects\ASP_Labs\PYProjectNew\Project_File.xlsx')
 
 print(dataset)
-#renaming the first column to date
-dataset_1 = dataset.rename(columns={'   ': 'Date'})
-print(dataset_1)
 
+#dimension of dataset
+dataset_dim = dataset.shape
+
+#renaming the first column to date
+dataset_2 = dataset.rename(columns={'   ': 'Date'})
+print(dataset_2)
+
+#removing the missing value
+dataset_3 = dataset_2.dropna()
+
+#checking the dimension
+dataset_3
+
+#creating region
+region_asia = dataset_3[['date','Brunei Darussalam','Indonesia','Malaysia',
+                    'Philippines','Thailand','Viet Nam','Myanmar',
+                    'Japan','Hong Kong','China','Taiwan',
+                    'Korea, Republic Of','India'
+                    ]]

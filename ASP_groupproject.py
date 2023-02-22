8d018ba91c35f2544fb55175066da70784998a69
Sheikkyyy <harishbawthan@gmail.com>
2023-02-22 14:33:08 +0800

    ASP_Project

--- ASP_groupproject.py
+++ ASP_groupproject.py
@@ -1,35 +1,55 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import matplotlib as plt
 
-dataset = pd.read_excel(r'C:\Users\haris\PycharmProjects\ASP_Labs\PYProjectNew\Project_File.xlsx')
+
+dataset = pd.read_excel(r"./Project_File.xlsx")
 
 #print(dataset)
 
 
 
-#dimension of dataset
+# dimension of dataset
 dataset_dim = dataset.shape
 
 
-#renaming the first column to date
-dataset_2 = dataset.rename(columns={dataset.columns[0]:'Date'})
+# renaming the first column to date
+dataset_2 = dataset.rename(columns={dataset.columns[0]:'year_month'})
 print(dataset_2)
 
-#removing white space in columns
+# To Do: removing white space in columns
 
+# split the year_month to year and month columns
+dataset_2["year"] = dataset_2["year_month"].str.split().str[0]
+dataset_2["month"] = dataset_2["year_month"].str.split().str[1]
 
-#creating region
-region_asia = dataset_2[['Date', ' Brunei Darussalam ', ' Indonesia ', ' Malaysia ',
+print(dataset_2)
+
+# creating region
+region_asia_df = dataset_2[['year', ' Brunei Darussalam ', ' Indonesia ', ' Malaysia ',
        ' Philippines ', ' Thailand ', ' Viet Nam ', ' Myanmar ', ' Japan ',
        ' Hong Kong ', ' China ', ' Taiwan ', ' Korea, Republic Of ', ' India ',
        ' Pakistan ', ' Sri Lanka ', ' Saudi Arabia ', ' Kuwait ', ' UAE ']]
 
-print(region_asia)
+
+#print(region_asia_df)
+
+# cast to integer
+#region_asia_df = region_asia_df.astype("int")
+region_asia_df = region_asia_df.astype({'year':'int'})
+
+#print(region_asia_df.dtypes)
 
 #filtering to year
-years_dataset = region_asia[360:480]
+years_dataset = region_asia_df[(region_asia_df["year"] >= 2008) & (region_asia_df["year"] <= 2017)]
 
-print(years_dataset)
+years_dataset = years_dataset.astype('int')
 
-#plotting of graph
+print(years_dataset.dtypes)
+
+
+#groupby
+#sum
+
+#plot
+#top 3

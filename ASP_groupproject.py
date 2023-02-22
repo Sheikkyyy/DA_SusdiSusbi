3978b8039fed3324231dbb798594c1fcf9d57f23
Sheikkyyy <harishbawthan@gmail.com>
2023-02-22 11:24:14 +0800

    ASP_Project

--- ASP_groupproject.py
+++ ASP_groupproject.py
@@ -4,25 +4,30 @@ import matplotlib as plt
 
 dataset = pd.read_excel(r'C:\Users\haris\PycharmProjects\ASP_Labs\PYProjectNew\Project_File.xlsx')
 
-print(dataset)
+#print(dataset)
+
+
 
 #dimension of dataset
 dataset_dim = dataset.shape
 
+
 #renaming the first column to date
-dataset_2 = dataset.rename(columns={'   ': 'Date'})
+dataset_2 = dataset.rename(columns={dataset.columns[0]:'Date'})
 print(dataset_2)
 
-#removing the missing value
-dataset_3 = dataset_2.dropna()
+#removing white space in columns
 
-#checking the dimension
-dataset_3.shape
 
 #creating region
-region_asia = dataset_3[['date','Brunei Darussalam','Indonesia','Malaysia',
-                    'Philippines','Thailand','Viet Nam','Myanmar',
-                    'Japan','Hong Kong','China','Taiwan',
-                    'Korea, Republic Of','India'
-                    ]]
+region_asia = dataset_2[['Date', ' Brunei Darussalam ', ' Indonesia ', ' Malaysia ',
+       ' Philippines ', ' Thailand ', ' Viet Nam ', ' Myanmar ', ' Japan ',
+       ' Hong Kong ', ' China ', ' Taiwan ', ' Korea, Republic Of ', ' India ',
+       ' Pakistan ', ' Sri Lanka ', ' Saudi Arabia ', ' Kuwait ', ' UAE ']]
 
+print(region_asia)
+
+#filtering to year
+years_dataset = region_asia[360:480]
+
+print(years_dataset)
